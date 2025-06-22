851e15f5781f9de57e2a2c681ade3c75d7c12bdf
Rawlings Mayabi <162916959+Seryabiz@users.noreply.github.com>
2025-06-22 09:54:26 +0300

    Web developed

--- app.py
+++ app.py
@@ -1,27 +1,48 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import joblib
 import sys
 import pandas as pd
 sys.path.append('./Notebooks')
 import Feature_Engineering
+import Preprocessing
 
 feature_pipeline = joblib.load('feature_engineering.pkl')
 
 columns = joblib.load('columns.pkl')
 
-pipeline = joblib.load('Preprocessing_pipeline.joblib')
+pipeline = joblib.load('Preprocessing_pipeline.pkl')
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def Home():
-    return render_template('index.html')
-@app.route('/predict', methods = ['POST'])
-def Prediction():
     model = joblib.load('model.pkl')
-    data = pd.DataFrame([request.json])
-    features = feature_pipeline.transform(data)
-    selected_features = features[[x for x in columns if x != 'rainfall']]
-    X = pipeline.transform(selected_features)
-    prediction = model.predict_proba(X)
-    return jsonify({'Probability of rain':prediction.tolist()[0][1]})
+    prediction = None
+    if request.method == 'POST':
+        # Extract data from the form
+        values = [
+            float(request.form['id']),
+            float(request.form['day']),
+            float(request.form['pressure']),
+            float(request.form['maxtemp']),
+            float(request.form['temparature']),
+            float(request.form['mintemp']),
+            float(request.form['dewpoint']),
+            float(request.form['humidity']),
+            float(request.form['cloud']),
+            float(request.form['sunshine']),
+            float(request.form['winddirection']),
+            float(request.form['windspeed'])
+        ]
+        data = pd.DataFrame([values],columns=['id','day','pressure','maxtemp',
+                                            'temparature','mintemp','dewpoint','humidity',
+                                            'cloud','sunshine','winddirection',
+                                            'windspeed'])
+        features = feature_pipeline.transform(data)
+        selected_features = features[[x for x in columns if x != 'rainfall']]
+        X = pipeline.transform(selected_features)
+        prediction = model.predict_proba(X)
+        prediction = prediction[0][1] 
+    return render_template('index.html',prediction=prediction)
+if __name__=='__main__':
+    app.run(debug=True)
